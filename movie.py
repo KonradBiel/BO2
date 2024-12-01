3d6b4946b6b7488e8a1daf51c2c630612c085eee
Jakub Gajewski <jgajewski@student.agh.edu.pl>
2024-12-01 19:09:31 +0100

    Jakies_klasy_tegasy_movie

--- movie.py
+++ movie.py
@@ -1,26 +1,24 @@
 from genres import GENRES_LIST
 import numpy as np
 
-data = []
-naglowki = ["tytuł", "Gatunek_1","Gatunek_2","Gatunek_3","czas","ocena"]
-with open('Baza_filmow_v2.csv', 'rt') as file:
-    for line in file:
-        record = line.rstrip().split(';')
-        dictionary = {}
-        for klucz, value in zip(naglowki, record):
-            dictionary[klucz] = value
-        dane.append(dictionary)
-print(data)
 
+# data = []
+# naglowki = ["tytuł", "Gatunek_1","Gatunek_2","Gatunek_3","czas","ocena"]
+# with open('Baza_filmow_v2.csv', 'rt') as file:
+#     for line in file:
+#         record = line.rstrip().split(';')
+#         data.append(record)
+# print(data)
 
 
 
 
 class Movie:
-    def __init__(self, title, rating, genres):
+    def __init__(self, title, rating, genres, watch_time):
         self.title = title
         self.rating = rating
         self.genres = genres
+        self.watch_time = watch_time
         self.create_genres_vector(self.genres)
 
     def create_genres_vector(self, genres):
@@ -46,6 +44,36 @@ class Movie:
                 self.genres_vec[ind2] = 2/np.sqrt(21)
                 self.genres_vec[ind3] = 1/np.sqrt(21)
 
+class Film_base:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.base =[]
 
-    def calculate_preference_correspondence(self, preference_vector):
-        pass
+    def set_film_base(self):
+        with open(f'{self.file_name}', 'rt') as file:
+            for line in file:
+                film = line.rstrip().split(';')
+                self.base.append(Movie[film[0],film[5],film[1:3],film[4]])
+                
+    set_film_base()
+
+
+
+class Platform:
+    def __init__(self, title, film_base, price):
+        self.title = title
+        self.film_base  = film_base 
+        self.price = price
+    
+    def set_price(self,price):
+        self.price = price
+
+
+
+
+def calculate_preference_correspondence(self, preference_vector):
+    pass
+
+
+A = Film_base('Baza_filmow_v2.csv')
+print(A)
