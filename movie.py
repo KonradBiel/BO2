9bd1387c95f10f0e22089b6f2461f6f5ed95123e
arturbrd <artur.ab2002@gmail.com>
2024-12-02 21:24:50 +0100

    implemented computing single platform score

--- movie.py
+++ movie.py
@@ -1,4 +1,4 @@
-from genres import GENRES_LIST
+from genres import ALL_GENRES
 import numpy as np
 
 
@@ -19,61 +19,50 @@ class Movie:
         self.rating = rating
         self.genres = genres
         self.watch_time = watch_time
+        self.score_cache = None
+        self.preference_reference = None
         self.create_genres_vector(self.genres)
 
     def create_genres_vector(self, genres):
         if len(genres) > 3:
             genres = genres[:3]
-        self.genres_vec = np.zeros(len(GENRES_LIST))
+        self.genres_vec = np.zeros(len(ALL_GENRES))
         match len(genres):
             case 0:
                 raise Exception("Movie doesn't have any genres")
             case 1:
-                ind = GENRES_LIST.index(genres[0])
+                ind = ALL_GENRES.index(genres[0])
                 self.genres_vec[ind] = 1
             case 2:
-                ind1 = GENRES_LIST.index(genres[0])
-                ind2 = GENRES_LIST.index(genres[1])
+                ind1 = ALL_GENRES.index(genres[0])
+                ind2 = ALL_GENRES.index(genres[1])
                 self.genres_vec[ind1] = 2/np.sqrt(5)
                 self.genres_vec[ind2] = 1/np.sqrt(5)
             case 3:
-                ind1 = GENRES_LIST.index(genres[0])
-                ind2 = GENRES_LIST.index(genres[1])
-                ind3 = GENRES_LIST.index(genres[2])
+                ind1 = ALL_GENRES.index(genres[0])
+                ind2 = ALL_GENRES.index(genres[1])
+                ind3 = ALL_GENRES.index(genres[2])
                 self.genres_vec[ind1] = 4/np.sqrt(21)
                 self.genres_vec[ind2] = 2/np.sqrt(21)
                 self.genres_vec[ind3] = 1/np.sqrt(21)
 
-class Film_base:
-    def __init__(self, file_name):
-        self.file_name = file_name
-        self.base =[]
-
-    def set_film_base(self):
-        with open(f'{self.file_name}', 'rt') as file:
-            for line in file:
-                film = line.rstrip().split(';')
-                self.base.append(Movie[film[0],film[5],film[1:3],film[4]])
-                
-    set_film_base()
-
-
-
-class Platform:
-    def __init__(self, title, film_base, price):
-        self.title = title
-        self.film_base  = film_base 
-        self.price = price
+    def __calculate_preference_correspondence(self, preference_vector):
+        if len(preference_vector) != len(self.genres_vec):
+            raise Exception("length of preference_vector doesn't match the length of genres_vec of the movie")
+        return np.dot(self.genres_vec, preference_vector)
     
-    def set_price(self,price):
-        self.price = price
+    def get_movie_score(self, preference_vector):
+        if self.score_cache is None or preference_vector is not self.preference_reference:
+            self.preference_reference = preference_vector
+            if self.rating < 40:
+                self.score_cache = 0;
+            elif self.rating < 90:
+                self.score_cache = self.__calculate_preference_correspondence(preference_vector)*(self.rating-40)/50
+            else:
+                self.score_cache = self.__calculate_preference_correspondence(preference_vector)
+        return self.score_cache
 
 
 
 
-def calculate_preference_correspondence(self, preference_vector):
-    pass
 
-
-A = Film_base('Baza_filmow_v2.csv')
-print(A)
