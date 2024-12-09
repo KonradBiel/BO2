ade9b6f10d7fe1473edbe72016f6de9dbf01c98f
KonradBiel <konbie@student.agh.edu.pl>
2024-12-09 01:21:17 +0100

    dodanie binarnej reprezentacji populacji

--- algorithm.py
+++ algorithm.py
@@ -3,6 +3,7 @@ import random
 NUM_Plamforms = 8 # liczba platform
 MUTATION_RATE = 0.1 # szansa na mutacje w zakresie od 0-1
 GENERATIONS = 50 # Liczba generacji
+population_size = 1
 
 def cost_function(platforms, preference_vector):
     cost_fun_val = 0
@@ -14,15 +15,16 @@ class Algorithm:
     def __init__(self, platforms, preference_vector):
         self.platforms = platforms
         self.platforms_len = len(platforms)
-        self.population = []
+        self.population = [0] * NUM_Plamforms
         self.preference_vector = preference_vector
 
-    def generate_initial_population(self, population_size):
-        for _ in range(population_size):
-            random_int = random.randint(0, 2**self.platforms_len-1)
-            self.population.append(Solution(self.__get_list_of_platforms_from_a_binary(random_int), self.preference_vector))
+    def generate_initial_population(self, population_size = population_size):
+        indices = random.sample(range(8), population_size)
+        for idx in indices:
+            self.population[idx] = 1
+        
 
-    def __get_list_of_platforms_from_a_binary(self, number):
+    def get_list_of_platforms_from_a_binary(self, number = population_size):
         platforms = []
         for i in range(self.platforms_len):
             if number%2 == 1:
@@ -44,10 +46,19 @@ class Algorithm:
         return muted_individual
     
     def evolotionary_algoritm(self, preference_vector):
-        population = self.generate_initial_population # stworzenie pierwszej generacji
+        self.generate_initial_population(population_size) # stworzenie pierwszej generacji
+        population = self.population
+        return population
+    
+    def print_platform_names(self, platform_base):
+        platform_names = []
 
-        for generation in range(GENERATIONS):
-            costs = [cost_function(population, preference_vector)]
+        for i in range(len(self.population)):
+            if self.population[i] == 1:
+                platform_names.append(platform_base[i].title)
+
+        print(platform_names)
+        
 
 
 
